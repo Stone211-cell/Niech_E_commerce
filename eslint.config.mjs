f639fffb749ccebeeb4d81a94839dc2915b9458c
Stone211-cell <suvapat1905@gmail.com>
2026-01-09 16:37:20 +0700

    go to versel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-=======
+
 import { defineConfig, globalIgnores } from "eslint/config";
->>>>>>> 12697e9ef6c88069bc3a6685cd881d6c342ceac1
+
 
 export default defineConfig([
   {
@@ -13,17 +13,17 @@ export default defineConfig([
     },
   },
   globalIgnores([
-<<<<<<< HEAD
+
     '.next/**',
     'out/**',
     'build/**',
     'next-env.d.ts',
-=======
+
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
     "lib/generated/prisma/**",
->>>>>>> 12697e9ef6c88069bc3a6685cd881d6c342ceac1
+
   ]),
 ]);
